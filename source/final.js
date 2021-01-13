a23ac3d794daa8fdf06e2af574f36f34d09bdd8f
Tongzhao9417 <42951960+Tongzhao9417@users.noreply.github.com>
2021-01-13 14:40:40 +0800

    Add files via upload

--- source/final.js
+++ source/final.js
@@ -28,34 +28,4 @@ $(function () {
         $('#8').show();
     },13000)
 
-
-    var obj = {};
-    function createObj(k,v) {
-        obj[k] = v
-        return obj;
-    }
-    $('.checkbox').click(function () {
-        $('#download').show();
-    })
-    var time = new Date();
-    var timestamp = Date.parse(time);
-    var now = moment(timestamp).locale('zh-cn').format('LLL')
-    function downloadData(data) {
-        var blob = new Blob([JSON.stringify(data)],{ type:"text/plain;charset=utf-8"});
-        saveAs(blob, now+'.txt');
-    }
-    for (var i = 0; i < sessionStorage.length; i++) {
-        var key = sessionStorage.key(i)
-        var value = sessionStorage.getItem(key)
-        createObj(key,value)
-    }
-    // for (var o = 0; o< arr.length; o++){
-    //     json[o] = arr[o]
-    // }
-    
-    // console.log(JSON.stringify(obj))
-    // var btn = document.getElementById('download')
-    $('#download').click(function (e) { 
-        downloadData(obj);
-    });
 });
