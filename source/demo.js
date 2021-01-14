14883c3e3551702d876abeccdc7cf078ff539ac8
Tongzhao9417 <42951960+Tongzhao9417@users.noreply.github.com>
2021-01-14 11:23:54 +0800

    Update demo.js

--- source/demo.js
+++ source/demo.js
@@ -25,7 +25,7 @@ $(function () {
     timer(intDiff);
     setTimeout (function (){
         $('#finishing').show()
-    },1000)
+    },61000)
     
     $('#finishing').click(function () { 
         sessionStorage.setItem('为评论点赞数量',countLikeNum())
@@ -42,7 +42,7 @@ $(function () {
         console.log(jsonStr)
         $.ajax({
             type: "post",
-            url: "http://tongzhao.xyz/get.php",
+            url: "https://tongzhao.xyz/get.php",
             // url: "http://localhost/TryV6/get2.php",
             async:true,
             contentType:'application/json',
